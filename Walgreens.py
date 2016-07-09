67cd8942812b598fcee93f616f8e4c2adcfb5f7e
jgscherber <jgscherber@gmail.com>
2016-07-09 10:45:39 -0500

    finished all the sections but one, forgot to grab the field ID before going to next, need to wait for survey timeout to get it again

--- Walgreens.py
+++ Walgreens.py
@@ -58,19 +58,88 @@ try:
 	submit.click()
 	answers = []
 	for i in range(10):
-		answers.append(random.randint(7,9))
-	browser.find_element_by_id("For_{0}".format(answers[0])).click()
+		answers.append(random.randint(6,8))
+	#first page
+	browser.find_element_by_id("RadioGroup{0}".format(answers[0])).click()
 	browser.find_element_by_id("ImageSubmit").click()
-	
-	browser.find_element_by_id("For_{0}".format(answers[1])).click()
+	#second page
+	browser.find_element_by_id("RadioGroup_28").click()
+	browser.find_element_by_id("RadioGroup_2_0{0}".format(answers[1])).click()
+	browser.find_element_by_id("RadioGroup_2_1{0}".format(answers[2])).click()
+	browser.find_element_by_id("RadioGroup_2_2{0}".format(answers[3])).click()
+	browser.find_element_by_id("RadioGroup_2_3{0}".format(answers[4])).click()
+	browser.find_element_by_id("RadioGroup_2_4{0}".format(answers[5])).click()
 	browser.find_element_by_id("ImageSubmit").click()
-	
-	browser.find_element_by_id("For_{0}".format(answers[2])).click()
+	#third page
+	browser.find_element_by_id("RadioGroup{0}".format(answers[6])).click()
 	browser.find_element_by_id("ImageSubmit").click()
-	
-	browser.find_element_by_id("For_{0}".format(answers[3])).click()
+	#forth page
+	browser.find_element_by_id("RadioGroup{0}".format(answers[7])).click()
 	browser.find_element_by_id("ImageSubmit").click()
+	#fifth page
+	browser.find_element_by_id("RadioGroup2").click()
+	browser.find_element_by_id("ImageSubmit").click()
+	#sixth page
+	browser.find_element_by_id("RadioGroup{0}".format(answers[8])).click()
+	browser.find_element_by_id("ImageSubmit").click()
+	#seventh page
+	browser.find_element_by_id("RadioGroup_28").click()
+	browser.find_element_by_id("RadioGroup_2_0{0}".format(answers[1])).click()
+	browser.find_element_by_id("RadioGroup_2_1{0}".format(answers[2])).click()
+	browser.find_element_by_id("RadioGroup_2_2{0}".format(answers[3])).click()
+	browser.find_element_by_id("ImageSubmit").click()
+	#eight page
+	browser.find_element_by_id("RadioGroup1").click()
+	browser.find_element_by_id("ImageSubmit").click()
+	#ninth page
+	browser.find_element_by_id("RadioGroup_28").click()
+	browser.find_element_by_id("ImageSubmit").click()
+	#tenth page
+	browser.find_element_by_id("RadioGroup1").click()
+	browser.find_element_by_id("ImageSubmit").click()
+	#eleventh page
+	browser.find_element_by_id("RadioGroup3").click()
+	browser.find_element_by_id("ImageSubmit").click()
+	#twelth page
+	browser.find_element_by_id("RadioGroup0").click()
+	browser.find_element_by_id("ImageSubmit").click()
+	#thirteenth page
+	browser.find_element_by_id("RadioGroup1").click()
+	browser.find_element_by_id("ImageSubmit").click()
+	#14th page
+	browser.find_element_by_id("RadioGroup{0}".format(answers[9])).click()
+	browser.find_element_by_id("ImageSubmit").click()
+	#15th page
+	browser.find_element_by_id("RadioGroup_28").click()
+	browser.find_element_by_id("RadioGroup_2_0{0}".format(answers[1])).click()
+	#16th page
+	browser.find_element_by_id("RadioGroup5").click()
+	browser.find_element_by_id("ImageSubmit").click()
+	#17th page
+	browser.find_element_by_id("RadioGroup1").click()
+	browser.find_element_by_id("ImageSubmit").click()
+	#18th page
+	#???
 	
+##	# 19th page
+##	browser.find_element_by_id("RadioGroup1").click()
+##	browser.find_element_by_id("ImageSubmit").click()
+##	# 20th page
+##	browser.find_element_by_id("RadioGroup2").click()
+##	browser.find_element_by_id("ImageSubmit").click()
+##	# 21st page
+##	browser.find_element_by_id("RadioGroup1").click()
+##	browser.find_element_by_id("ImageSubmit").click()
+##	# 22nd page
+##	browser.find_element_by_id("RadioGroup0").click()
+##	browser.find_element_by_id("ImageSubmit").click()
+##	# final page
+##	browser.find_element_by_id("promptInput_76122").send_keys('Jacob Scherber')
+##	browser.find_element_by_id("promptInput_76123").send_keys('6127090685')
+##	browser.find_element_by_id("promptInput_76124").send_keys('1010 Lake St NE Apt 222')
+##	browser.find_element_by_id("promptInput_76125").send_keys('Hopkins')
+##	browser.find_element_by_id("promptInput_76126").send_keys('MN')
+##	browser.find_element_by_id("promptInput_76127").send_keys('55343')
 except:
 	browser.close()
 	print("The codes have expired")
