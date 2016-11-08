d2e4b979219dcd9bb144c9794bbf9d88f8a9a5b2
Jacob Scherber <jgscherber@gmail.com>
2016-11-08 09:49:14 -0600

    noted a possible solution

--- ProjEuler/Python/reciprocal_cycle.py
+++ ProjEuler/Python/reciprocal_cycle.py
@@ -19,3 +19,4 @@ Find the value of d < 1000 for which 1/d contains the longest recurring cycle
 in its decimal fraction part.
 """
 
+# https://en.wikipedia.org/wiki/Longest_repeated_substring_problem
