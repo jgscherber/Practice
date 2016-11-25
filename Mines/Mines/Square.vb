d594b4669e430e727089d598c1d5fd9e056002b8
jgscherber <jgscherber@gmail.com>
2016-11-25 15:37:05 -0600

    more ch4 project work

--- Mines/Mines/Square.vb
+++ Mines/Mines/Square.vb
@@ -7,8 +7,17 @@
         Mine
     End Enum
 
+    ' Create immutable variables (Const)
+    Public Const ShowMine As String = "@"
+    Public Const ShowFlag As String = "F"
+    Public Const ShowBrokenFlag As String = "X"
+
+    Private contents As HiddenValue ' what does the square hold? (Enum above)
+    Private actualNearMines As Integer ' how many nearby mines?
+
+
     Dim Row, Col As Integer 'global variables in the class
-    ' initialization method
+    ' initialization subroutine
     Public Sub New(R As Integer, C As Integer)
         ' MyBase is the super class, create a new super class object
         MyBase.New()
@@ -23,7 +32,7 @@
         Width = 30
         Text = ""
         FlatStyle = System.Windows.Forms.FlatStyle.Standard
-
+        contents = HiddenValue.Uninitialized ' Enums like self-contain class attributes (dot operator)
 
     End Sub
 
