82cabdced1edf6c29605dc7cc8fe1ca5096919e4
Jacob Scherber <jgscherber@gmail.com>
2016-11-27 11:34:10 -0600

    reset files to old hw

--- scher313_10A.py
+++ scher313_10A.py
@@ -23,22 +23,48 @@ class FootMeasure(object):
         # need to work on this - using a print statement still returns None
         if ((self.feet*12) + self.inches) - ((other.feet*12) + other.inches) \
            < 0:
-            raise ValueError("Error: subtraction results in a negative value of " \
-                  + str(FootMeasure(inches = ((self.feet*12) + self.inches) \
-                           - ((other.feet*12) + other.inches))))
+##            raise ValueError("Error: subtraction results in a negative value of " \
+##                  + str(FootMeasure(inches = ((self.feet*12) + self.inches) \
+##                           - ((other.feet*12) + other.inches))))
+           pass
         else:
             return FootMeasure(inches = ((self.feet*12) + self.inches) \
                            - ((other.feet*12) + other.inches))
-    
-meas1 = FootMeasure()
-meas2 = FootMeasure(feet = 5)
-meas3 = FootMeasure(feet = 5, inches = 8)
-meas4 = FootMeasure(inches = 68)
 
-"""
-meas5 = meas2 + meas3
-meas5
-meas3 + meas4
-meas3 - meas4
-meas6 = meas2 - meas3
-"""
+    def __lt__(self, other):
+        return (self.feet*12) + self.inches \
+                           < (other.feet*12) + other.inches
+    def __gt__(self, other):
+        return (self.feet*12) + self.inches \
+                           > (other.feet*12) + other.inches
+    def __eq__(self, other):
+        return (self.feet*12) + self.inches \
+                           == (other.feet*12) + other.inches
+def testMeasure(): 
+    meas1 = FootMeasure()
+    meas2 = FootMeasure(feet = 5)
+    meas3 = FootMeasure(feet = 5, inches = 8)
+    meas4 = FootMeasure(inches = 68)
+    meas7 = FootMeasure(inches = 5)
+    print(meas2)
+    print(meas4)
+    print(meas7)
+    print()
+    meas5 = meas2 + meas3
+    print(meas5)
+    print(meas3 + meas4)
+    print(meas3 - meas4)
+    # broken subtraction
+    meas6 = meas2 - meas3
+    print(meas6)
+    print()
+    print(meas2 < meas3)
+    print(meas2 > meas3)
+    print(meas3 < meas2)
+    print(meas3 > meas2)
+    meas8 = FootMeasure(feet = 5)
+    print(meas2 == meas8)
+    print(meas2 == meas3)
+    
+testMeasure()
+
