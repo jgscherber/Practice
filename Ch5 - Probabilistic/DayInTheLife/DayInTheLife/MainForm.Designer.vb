6dfeeffb70f4057417cebfe56df34c839244d65f
jgscherber <jgscherber@gmail.com>
2016-12-23 15:54:45 -0600

    finished ch45 exercise. short chapter

--- Ch5 - Probabilistic/DayInTheLife/DayInTheLife/MainForm.Designer.vb
+++ Ch5 - Probabilistic/DayInTheLife/DayInTheLife/MainForm.Designer.vb
@@ -24,6 +24,11 @@ Partial Class MainForm
     Private Sub InitializeComponent()
         Me.EddyButton = New System.Windows.Forms.Button()
         Me.ThoughtsTextBox = New System.Windows.Forms.TextBox()
+        Me.GaryButton = New System.Windows.Forms.Button()
+        Me.MikeButton = New System.Windows.Forms.Button()
+        Me.CarlButton = New System.Windows.Forms.Button()
+        Me.LarryButton = New System.Windows.Forms.Button()
+        Me.BarryButton = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'EddyButton
@@ -46,11 +51,61 @@ Partial Class MainForm
         Me.ThoughtsTextBox.Size = New System.Drawing.Size(807, 384)
         Me.ThoughtsTextBox.TabIndex = 1
         '
+        'GaryButton
+        '
+        Me.GaryButton.Location = New System.Drawing.Point(13, 43)
+        Me.GaryButton.Name = "GaryButton"
+        Me.GaryButton.Size = New System.Drawing.Size(75, 23)
+        Me.GaryButton.TabIndex = 2
+        Me.GaryButton.Text = "Gary"
+        Me.GaryButton.UseVisualStyleBackColor = True
+        '
+        'MikeButton
+        '
+        Me.MikeButton.Location = New System.Drawing.Point(13, 73)
+        Me.MikeButton.Name = "MikeButton"
+        Me.MikeButton.Size = New System.Drawing.Size(75, 23)
+        Me.MikeButton.TabIndex = 3
+        Me.MikeButton.Text = "Mike"
+        Me.MikeButton.UseVisualStyleBackColor = True
+        '
+        'CarlButton
+        '
+        Me.CarlButton.Location = New System.Drawing.Point(13, 103)
+        Me.CarlButton.Name = "CarlButton"
+        Me.CarlButton.Size = New System.Drawing.Size(75, 23)
+        Me.CarlButton.TabIndex = 4
+        Me.CarlButton.Text = "Carl"
+        Me.CarlButton.UseVisualStyleBackColor = True
+        '
+        'LarryButton
+        '
+        Me.LarryButton.Location = New System.Drawing.Point(13, 133)
+        Me.LarryButton.Name = "LarryButton"
+        Me.LarryButton.Size = New System.Drawing.Size(75, 23)
+        Me.LarryButton.TabIndex = 5
+        Me.LarryButton.Text = "Larry"
+        Me.LarryButton.UseVisualStyleBackColor = True
+        '
+        'BarryButton
+        '
+        Me.BarryButton.Location = New System.Drawing.Point(13, 163)
+        Me.BarryButton.Name = "BarryButton"
+        Me.BarryButton.Size = New System.Drawing.Size(75, 23)
+        Me.BarryButton.TabIndex = 6
+        Me.BarryButton.Text = "Barry"
+        Me.BarryButton.UseVisualStyleBackColor = True
+        '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(914, 411)
+        Me.Controls.Add(Me.BarryButton)
+        Me.Controls.Add(Me.LarryButton)
+        Me.Controls.Add(Me.CarlButton)
+        Me.Controls.Add(Me.MikeButton)
+        Me.Controls.Add(Me.GaryButton)
         Me.Controls.Add(Me.ThoughtsTextBox)
         Me.Controls.Add(Me.EddyButton)
         Me.Name = "MainForm"
@@ -62,4 +117,9 @@ Partial Class MainForm
 
     Friend WithEvents EddyButton As Button
     Friend WithEvents ThoughtsTextBox As TextBox
+    Friend WithEvents GaryButton As Button
+    Friend WithEvents MikeButton As Button
+    Friend WithEvents CarlButton As Button
+    Friend WithEvents LarryButton As Button
+    Friend WithEvents BarryButton As Button
 End Class
--- Ch5 - Probabilistic/DayInTheLife/DayInTheLife/Person.vb
+++ Ch5 - Probabilistic/DayInTheLife/DayInTheLife/Person.vb
@@ -1,3 +1,88 @@
-﻿Public Class Person
+﻿Imports DayInTheLife
+
+Public MustInherit Class Person ' must inherit ensures that this is alway an abstract class (can't be instantiated by itself)
+    Public Function Pick(Cash As Double, Occupations As Collection) As Job ' picks a job and then returns which one it did
+        Dim bestJob As Job = CType(Occupations(1), Job)
+        Dim bestValue As Double = Me.Evaluate(bestJob, Cash)
+
+        Dim otherJob As Job
+        Dim otherValue As Double
+        For Each otherJob In Occupations
+            If 2.0 * otherJob.Cost <= Cash Then
+                otherValue = Me.Evaluate(otherJob, Cash)
+                If otherValue > bestValue Then
+                    bestJob = otherJob
+                    bestValue = otherValue
+                End If
+            End If
+        Next
+        Return bestJob
+    End Function
+
+    Public MustOverride Function Evaluate(Task As Job, Cash As Double) As Double
 
 End Class
+
+Public Class Eddy ' not good to subclass these, would be better to type them and instatiate them as names (e.g. Eddy is of the BalancePerson type)
+    Inherits Person
+
+    Public Overrides Function Evaluate(Task As Job, Cash As Double) As Double
+        Return Task.PSuccess * Task.PSuccess * Task.Gain _
+            - (1 - Task.PSuccess) * Task.Loss
+    End Function
+End Class
+
+Public Class Gary ' PotentialOnlyPerson
+    Inherits Person
+
+    Public Overrides Function Evaluate(Task As Job, Cash As Double) As Double
+        Return Task.Gain
+    End Function
+End Class
+
+Public Class Mike ' CostWorriesPerson
+    Inherits Person
+
+    Public Overrides Function Evaluate(Task As Job, Cash As Double) As Double
+        Return -Task.Cost
+    End Function
+End Class
+
+Public Class Carl ' EasyMoneyPerson
+    Inherits Person
+
+    Public Overrides Function Evaluate(Task As Job, Cash As Double) As Double
+        Return Task.PSuccess * Task.Gain
+    End Function
+End Class
+
+Public Class Larry ' BigTimePerson
+    Inherits Person
+
+    Public Overrides Function Evaluate(Task As Job, Cash As Double) As Double
+        Return Task.PSuccess * Task.Gain - (1 - Task.PSuccess) * Task.Loss
+    End Function
+End Class
+
+Public Class Barry ' BolderBalancePerson
+    Inherits Person
+
+    Public Overrides Function Evaluate(Task As Job, Cash As Double) As Double
+        Return Task.PSuccess * Task.PSuccess * Task.Gain - (1 - Task.PSuccess) _
+            * (1 - Task.PSuccess) * Task.Loss
+    End Function
+End Class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
