1b8b110604731f1790a1c436fcb5940de436c3ee
jgscherber <jgscherber@gmail.com>
2016-12-23 22:38:52 -0600

    started ch6 exercise

---
+++ Ch6 - Planning/Fox and Hounds/Fox and Hounds/FaHButton.vb
@@ -0,0 +1,21 @@
+﻿Public Class FaHButton
+    Inherits Button
+
+    Private MySubscript As Integer ' only this class has access to this variable
+    Protected HoundNumber As Integer ' available to this class and subclasses
+
+    Public ReadOnly Property Subscript() As Integer
+        Get
+            Return MySubscript
+        End Get
+    End Property
+
+    Public Sub New(ss As Integer)
+        MySubscript = ss
+
+        AllowDrop = True ' attribute of the Button class that it's inheriting from
+        Me.Font = New System.Drawing.Font("Arial", 9, FontStyle.Bold)
+    End Sub
+
+
+End Class
