c51b4daae5ca38d6c171eac8722aee1cdcfb872e
JacobCSE <jgscherber@gmail.com>
2017-01-17 18:37:39 -0600

    finished with exercise 5.3

--- Java/Chapter 5 - Exercises/src/DiceWithStatCalc_53.java
+++ Java/Chapter 5 - Exercises/src/DiceWithStatCalc_53.java
@@ -11,20 +11,35 @@ public class DiceWithStatCalc_53 {
 	 */
 	public static void main(String[] args) {
 		StatCalc[] data = new StatCalc[11]; // array  to hold the data for each experiment
+		PairOfDice2 dice = new PairOfDice2();
 		for (int face = 2; face < 13; face++) { // run the experiment for each die
+			
 			int trial = 0;
-			while (trial < 10000) {
-				
-				
-				
-				
-				
-			} // end trial
-			
-			
-			
-			
+			int index = face - 2; // shift dice faces to index positions
+			data[index] = new StatCalc(); // create a StatCalc object for the current face
+			while (trial < 100000) {
+				int rTotal = 0; // holds the die roll total on each attempt
+				int rCount = 0; // counts the number of rolls before success
+				while (rTotal != face) // roll until get current face
+				{
+					dice.roll();
+					rTotal = dice.getDie1() + dice.getDie2();
+					rCount ++;
+					  
+				}
+				data[index].enter(rCount); // add the previous trial to the db
+				trial ++;
+			} // end trial			
 		} // end faces
+		
+		for (int i = 0; i < 11; i++){
+			System.out.println("____ Data for dice roll " + (i+2) + " ____");
+			System.out.println("Average: " + data[i].getMean());
+			System.out.println("Max: " + data[i].getMax());
+			System.out.println("Std. Dev: " + data[i].getStandardDeviation());			
+			
+		} // end print
+		
 
 	} // end main
 
