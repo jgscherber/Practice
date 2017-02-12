90a3a2c865c371afa55c32454278b54b51f7ed1a
JacobLaptop <jgscherber@gmail.com>
2017-02-11 19:11:10 -0600

    worked on part 5(?)

---
+++ Java/DIY Java Games/Watch Your Step/src/jacob/scherber/watchyourstep/TerrainButton.java
@@ -0,0 +1,89 @@
+package jacob.scherber.watchyourstep;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+
+public class TerrainButton extends JButton {
+
+	private static final long serialVersionUID = 1L;
+	
+	private static final int SIZE = 50;
+	
+	private int row=0;
+	private int col=0;
+	private int nextToHoles=0;
+	
+	private boolean revealed = false;
+	private boolean hole = false;
+	
+	public TerrainButton(int row, int col) {
+		this.row = row;
+		this.col = col;
+		Dimension size = new Dimension(SIZE,SIZE);
+		setPreferredSize(size);
+		
+		
+		
+	}
+	
+	// getters
+	public int getRow() { 
+		return row;
+	}
+	
+	public int getCol() {
+		return col;
+	}
+	
+	public boolean hasHole() {
+		return hole;
+	}
+	
+	public boolean isRevealed() {
+		return revealed;
+	}
+	
+	// setters
+	public void setHole(boolean hasHole) {
+		hole = hasHole;
+	}
+
+	public void increaseHoleCount() {
+		nextToHoles++;
+	}
+	
+	public boolean isNextToHoles() {
+		return nextToHoles > 0;
+	}
+	
+	public void reveal(boolean reveal) {
+		revealed = reveal;
+		if (revealed) {
+			if(hole) setBackground(Color.BLACK);
+			else setBackground(Color.CYAN);
+		
+			if (isNextToHoles()) setText(Integer.toString(nextToHoles));
+		}
+		else {
+			setBackground(null);
+			setText("");
+		}
+	} //end reveal
+
+	public void reset() {
+		row=0;
+		col=0;
+		nextToHoles=0;
+		
+		revealed = false;
+		hole = false;
+		
+		setBackground(null);
+		setText("");
+	} // end reset
+
+
+
+} //end class
--- Java/DIY Java Games/Watch Your Step/src/jacob/scherber/watchyourstep/WatchYourStep.java
+++ Java/DIY Java Games/Watch Your Step/src/jacob/scherber/watchyourstep/WatchYourStep.java
@@ -1,17 +1,33 @@
 package jacob.scherber.watchyourstep;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
 
 import jacob.scherber.mycomponents.TitleLabel;
 
 public class WatchYourStep extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
+	private static final int GRIDSIZE = 10;
+	private final int NUMBEROFHOLES = 10; // "instance" = non static
+	private int totalRevealed = 0;
 	
+	private TerrainButton[][] terrain = new TerrainButton[GRIDSIZE][GRIDSIZE];
 	public WatchYourStep() {
 		initGUI();
+		
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		pack();
 		setVisible(true);
 		
@@ -20,16 +36,40 @@ public class WatchYourStep extends JFrame {
 	}
 	
 	public void initGUI() {
+		
 		TitleLabel titleLabel = new TitleLabel("Watch Your Step");
 		add(titleLabel, BorderLayout.PAGE_START);
 		
+		JPanel centerPanel = new JPanel(new GridLayout(GRIDSIZE,GRIDSIZE));
+		add(centerPanel, BorderLayout.CENTER);	
 		
-	}
+		for(int row = 0; row<GRIDSIZE; row++) {
+			for(int col = 0; col < GRIDSIZE; col++) {
+				terrain[row][col] = new TerrainButton(row, col);
+				centerPanel.add(terrain[row][col]);				
+			}			
+		}
+		
+	}// end initGUI
 	
 	
 	public static void main(String[] args) {
-		new WatchYourStep();
-
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				new WatchYourStep();
+				
+			}
+		});
+		
+		try{
+			String classname = UIManager.getCrossPlatformLookAndFeelClassName();
+			UIManager.setLookAndFeel(classname);
+		}
+		catch (Exception e) {}
+				
+		
 	}
 
 }
