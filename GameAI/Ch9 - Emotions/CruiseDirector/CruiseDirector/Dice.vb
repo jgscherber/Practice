f7e9c7e8e9fa13bae18159fa626c7e05b686ccab
Jacob Scherber <jgscherber@gmail.com>
2017-02-26 18:06:47 -0600

    Revert "moved GameAI into this repository and into its own folder"
    This reverts commit 95d52de54e3ad512812ebe64f3541d9e665f41a3, reversing
    changes made to 2f33bf00da93a434534147f810d56b5fcccc45d0.
    Removing GameAI, too many random files

--- GameAI/Ch9 - Emotions/CruiseDirector/CruiseDirector/Dice.vb
+++
@@ -1,5 +0,0 @@
-﻿Module Dice
-    Public Function getDx(dots As Integer) As Integer
-        Return CInt(Int((Rnd() * dots) + 1))
-    End Function
-End Module
--- GameAI/Ch9 - Emotions/CruiseDirector/CruiseDirector/NVP.vb
+++
@@ -1,4 +0,0 @@
-﻿Public Class NVP ' needs for a person
-    Public name As String
-    Public value As Double
-End Class
