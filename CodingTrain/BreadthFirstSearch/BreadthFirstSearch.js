27cfcdfb79a13b0b61ce47ddf69c92e79a72fac0
Jacob Scherber <jgscherber@gmail.com>
2017-04-02 14:41:42 -0500

    done up to 13 minutes

--- CodingTrain/BreadthFirstSearch/BreadthFirstSearch.js
+++ CodingTrain/BreadthFirstSearch/BreadthFirstSearch.js
@@ -1,10 +1,26 @@
+var data;
+var graph;
+
+function preload() {
+ data = loadJSON('kevinbacon.json'); 
+}
+
 function setup() {
-    createCanvas(400,400);
-    console.log('')
+    noCanvas();
+    graph = new Graph();
+    
+    var movies = data.movies;
+    
+    for(var i = 0; i < movies.length; i++) {
+     var movie = movies[i].title;
+     var cast = movies[i].cast;
+     var n = new Node(movie);
+     graph.addNode(n);
+    }
     
 } // end setup
 
-function draw() {
-  background(0);
+//function draw() {
+//  background(0);
   
-} // end draw
+//} // end draw
---
+++ CodingTrain/BreadthFirstSearch/Graph.js
@@ -0,0 +1,10 @@
+function Graph() {
+  this.nodes = [];
+  this.array = {};
+  
+}
+
+Graph.prototype.addNode = function(n) {
+  this.nodes.push(n);
+  // 13:00
+}
