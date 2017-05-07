2c21dfc33c49e455713d02bc0eed3869a7f781f3
Jacob Scherber <jgscherber@gmail.com>
2017-05-07 11:42:17 -0500

    loc 1717

--- Java/AdvancedDIY/Speed Words/src/speedwords/GamePanel.java
+++ Java/AdvancedDIY/Speed Words/src/speedwords/GamePanel.java
@@ -2,6 +2,9 @@ package speedwords;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -12,17 +15,24 @@ import jacob.scherber.mycomponents.FileIO;
 public class GamePanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	
+	// board sizing
 	private static final int WIDTH = 500;
 	private static final int HEIGHT = 300;
 	
 	private static final int START_X = WIDTH/2 - (LetterTile.SIZE * 7)/2;
 	private static final int  START_Y = HEIGHT/2 - (LetterTile.SIZE/2);
 	
+	// word selection
 	private static final String FILE_NAME = "/enable1_7.txt";
 	private ArrayList<TileSet> tileSets = new ArrayList<TileSet>();
 	private ArrayList<String> sevenLetterWords = new ArrayList<String>();
 	
 	private Random rand = new Random();
+	
+	// tile moving
+	private TileSet movingTiles;
+	private int mouseX;
+	private int mouseY;
 	
 	private SpeedWords speedWords;
 	
@@ -30,19 +40,124 @@ public class GamePanel extends JPanel {
 		this.speedWords = speedWords;
 		sevenLetterWords = FileIO.readTextFile(this, FILE_NAME);
 		restart();
+		
+		// clicking
+		addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				int x = e.getX();
+				int y = e.getY();
+				int mouseButton = e.getButton();
+				// left click moves single tile, right moves whole tileset
+				boolean leftClicked = mouseButton == MouseEvent.BUTTON1;
+				clicked(x,y,leftClicked);
+			}
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				released();
+			}
+		});
+		// dragging
+		addMouseMotionListener(new MouseMotionAdapter() {
+			
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				int x = e.getX();
+				int y = e.getY();
+				dragged(x,y);
+			}
+			
+		});
+	}// end GamePanel()
+	
+	private void dragged(int x, int y) {
+		if(x>0 && x < WIDTH && 
+				y > 0 && y < HEIGHT){
+			if(movingTiles != null) {
+				int changeX = x - mouseX;
+				int changeY = y - mouseY;
+				movingTiles.changeXY(changeX, changeY);
+				mouseX = x;
+				mouseY = y;
+				repaint();
+			}
+		}
 	}
+
+	private void released() {
+		if(movingTiles != null) {
+			String s = movingTiles.toString();
+			int x = movingTiles.getX();
+			int y = movingTiles.getY();
+			TileSet newTileSet = new TileSet(s,x,y);
+			tileSets.add(0,newTileSet);
+			movingTiles = null;
+			repaint();
+		}		
+		// tile dropped on top of another - connect them
+		if(movingTiles != null) {
+			boolean addedToTiles = false;
+			for(int i = 0; i < tileSets.size() && !addedToTiles; i++) {
+				TileSet tileSet = tileSets.get(i);
+				addedToTiles = tileSet.insertTiles(movingTiles);
+				if(addedToTiles) {
+					movingTiles = null;
+				}
+			}
+		}
+		// not dropped on top of another - add back to list of tileSets
+		
+	}
+
+	private void clicked(int x, int y, boolean leftClicked) {
+		
+			// no other movingTiles already selected
+			if(movingTiles == null) {
+				mouseX = x;
+				mouseY = y;
+				
+				for(int i = 0; i < tileSets.size() && movingTiles == null; i++) {
+					TileSet tileSet = tileSets.get(i);
+					if(tileSet.contains(mouseX, mouseY)) {
+						if(leftClicked) {
+							movingTiles = tileSet.removeAndReturn1TileAt(mouseX, mouseY);
+							if(tileSet.getNumberofTiles() == 0) {
+								tileSets.remove(i);
+							}
+						} else {
+							movingTiles = tileSet;
+							tileSets.remove(i);
+						}
+					}				
+				}
+				
+				repaint();
+				
+			}// end null check
+		}
+	
 	
 	@Override
-	protected void paintComponent(Graphics g) {
+	public void paintComponent(Graphics g) {
+		
+		
 		// drawing the background
 		g.setColor(SpeedWords.TAN);
-		g.drawRect(0, 0, WIDTH, HEIGHT);
+		g.fillRect(0, 0, WIDTH, HEIGHT); // was only drawing a rectangle, not filling, not covering junk in background
 		
         // drawing all curent tile sets
-		for(TileSet tileSet : tileSets) {
-			tileSet.draw(g);
+		for(int i = tileSets.size() - 1; i > -1; i--) {
+			tileSets.get(i).draw(g);
 		}
-
+		
+		// drawing moving tiles
+		if(movingTiles != null) {
+			// uses TileSet's draw (useful definition of a "container" class)
+			movingTiles.draw(g);
+		}
+		
     }
 	
 	@Override
--- Java/AdvancedDIY/Speed Words/src/speedwords/TileSet.java
+++ Java/AdvancedDIY/Speed Words/src/speedwords/TileSet.java
@@ -23,8 +23,8 @@ public class TileSet {
 	}
 	
 	public void draw(Graphics g) {
-		for(int i = 0; i < tiles.size(); i ++) {
-			LetterTile tile = tiles.get(i);
+		for(int i = 0; i < getNumberofTiles(); i ++) {
+			LetterTile tile = getTile(i);
 			int xPos = x + (LetterTile.SIZE * i);
 			// draw each tile using it's draw method
 			tile.draw(g, xPos, y);
@@ -37,7 +37,7 @@ public class TileSet {
 	}
 	
 	private int getWidth() {
-		int width = tiles.size() * LetterTile.SIZE;
+		int width = getNumberofTiles() * LetterTile.SIZE;
 		return width;
 	}
 	
@@ -52,6 +52,94 @@ public class TileSet {
 		}
 		return contains;
 	}
+	
+	public String toString() {
+		String s = "";
+		for(int i = 0; i < getNumberofTiles(); i++) {
+			s += getTile(i).getLetter();
+		}
+		return s;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public int getNumberofTiles() {
+		return tiles.size();
+	}
+	
+	public LetterTile getTile(int i) {
+		return tiles.get(i);
+	}
+	
+	public TileSet removeAndReturn1TileAt(int clickedX, int clickedY) {
+		TileSet tileSet = null;
+		for(int i = 0; i < getNumberofTiles() && tileSet == null; i ++) {
+			// tile boundaries
+			int tileLeftEdge = x + (LetterTile.SIZE * i);
+			int tileRightEdge = tileLeftEdge + LetterTile.SIZE;
+			int tileTopEdge = y;
+			int tileBottomEdge = tileTopEdge + LetterTile.SIZE;
+			// click within tile area
+			if(clickedX >= tileLeftEdge && clickedX <= tileRightEdge &&
+					clickedY >= tileTopEdge && clickedY <= tileBottomEdge) {
+				LetterTile tile = tiles.get(i);
+				String letter = tile.getLetter();
+				tileSet = new TileSet(letter, tileLeftEdge, tileTopEdge);
+				tiles.remove(i);
+				if(i == 0) {
+					x += LetterTile.SIZE;
+				}
+			
+				
+			}
+		}
+		return tileSet;
+	}
+	
+	public boolean insertTiles(TileSet droppedTiles) {
+		boolean inserted = false;
+		// tile location information
+		int droppedTilesWidth = droppedTiles.getWidth();
+		int droppedTilesHeight = LetterTile.SIZE;
+		int droppedTilesLeft = droppedTiles.getX();
+		int droppedTilesRight = droppedTilesLeft + droppedTilesWidth;
+		int droppedTilesTop = droppedTiles.getY();
+		int droppedTilesBottom = droppedTilesTop + droppedTilesHeight;
+		// check if the dropped tiles over the row
+		if(droppedTilesBottom > y && droppedTilesTop < (y+droppedTilesHeight)) {
+			// check if overlaps the left edge of 1st tile
+			if(droppedTilesRight >= x && droppedTilesRight <= (x + LetterTile.SIZE)){
+				// insert dropped tile before the one they were dropped on
+				for(int i = 0; i < droppedTiles.getNumberofTiles(); i++) { // add them all
+					LetterTile tile = droppedTiles.getTile(i); // (inmutable access to internal ArrayList)
+					tiles.add(i, tile);
+					inserted = true;
+				}
+				x -= droppedTilesWidth;
+			} else {
+				// check if they overlap other tiles
+				for(int i = 0; i < tiles.size() && !inserted; i++) {
+					int compareX = x + (i * LetterTile.SIZE);
+					if(droppedTilesLeft >- compareX && droppedTilesLeft <= (compareX + LetterTile.SIZE)) {
+						// insert after first overlapped tile
+						for(int j = 0; j < droppedTiles.getNumberofTiles(); j++) {
+							LetterTile tile = droppedTiles.getTile(j);
+							tiles.add(i+j+1,tile);
+							inserted = true;
+						}
+					}
+				}
 
-
+					
+			}// end horizonatal check
+		}// end verticle check
+		return inserted;
+		
+	}
 }
