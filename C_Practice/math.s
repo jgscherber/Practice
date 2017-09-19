51cc12eafe800fa24ce0fa68d77eac5b458899c9
Jacob Scherber <scher313@umn.edu>
2017-09-19 14:50:59 -0500

    java practice, unity messed up

--- C_Practice/math.s
+++ C_Practice/math.s
@@ -6,28 +6,32 @@
 	.globl	main
 	.type	main, @function
 main:
-.LFB0:
+.LFB0: ; labels, usually referenced by exception-handling related code
+	; .cfi* are used by compiler to help traverse stack for exception handling
 	.cfi_startproc
-	pushq	%rbp
+	pushq	%rbp ; push the base point of the stack (quadword)
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	subq	$32, %rsp
-	movl	$3, -20(%rbp)
-	movl	$5, -16(%rbp)
-	movl	-20(%rbp), %edx
-	movl	-16(%rbp), %eax
-	addl	%edx, %eax
-	movl	%eax, -12(%rbp)
-	movl	$15, -8(%rbp)
-	movl	$0, -4(%rbp)
-	movl	-20(%rbp), %eax
-	movl	%eax, %esi
-	movl	$.LC0, %edi
-	movl	$0, %eax
-	call	printf
-	movl	$0, %eax
+	subq	$32, %rsp ; subtrack 32 from stack pointer (4 * 8) but 5 ints defined?
+	movl	$3, -20(%rbp) ; a = 3
+	movl	$5, -16(%rbp) ; b = 5;
+	movl	-20(%rbp), %edx ; move value of a to %edx (bytes 5-8 of register %rdx)
+	movl	-16(%rbp), %eax ; move value of b to %eax (byte 5-8 of %rax)
+	addl	%edx, %eax ; a + b , saved in %eax
+	movl	%eax, -12(%rbp) ; move to c ( -12(%rbp))
+	movl	$15, -8(%rbp) ; compiler did the 3 * 5
+	movl	$0, -4(%rbp) ; and the 3/5
+	movl	-20(%rbp), %eax ; move value of a to last 4 byes of %rax
+	movl	%eax, %esi ; move to last 4 bytes of %rsi
+	movl	$.LC0, %edi ; move thing at jump .LCO (.string("%d")) to %edi
+	movl	$0, %eax ; clear %eax
+	call	printf ; printf does it's thing
+	movl	$0, %eax ; clear %eax again
+	; leave is equivalent to: (undoes the pushq and movq at the top)
+	; movq %rbp, %rsp
+	; popq %rbp
 	leave
 	.cfi_def_cfa 7, 8
 	ret
